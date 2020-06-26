87a662c88a81faa7b37659148a4015eb43ff3993
Ismael de Oliveira <ismael.oliveira.1997@hotmail.com>
2020-06-26 19:43:59 -0300

    create super user

--- mymemories/mymemories_app/admin.py
+++ mymemories/mymemories_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User, Address, Person, Administrador, Like, Comment
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Address)
+admin.site.register(Person)
+admin.site.register(Administrador)
+admin.site.register(Like)
+admin.site.register(Comment)
